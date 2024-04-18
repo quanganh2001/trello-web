88ecd0683fd9121308855986d49fcc16d22b4c31
quanganh2001 <cocoi5659@gmail.com>
2024-04-18 17:44:24 +0700

    Trello Board Bar Coding: Add icon chip component, add groped avatar icon and add invite user icon, create add icon

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,15 +1,151 @@
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import { Tooltip } from '@mui/material'
+import Button from '@mui/material/Button'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+
+const MENU_STYLES = {
+  color: 'primary.main',
+  bgcolor: 'white',
+  border: 'none',
+  paddingX: '5px',
+  borderRadius: '4px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
 
 function BoardBar() {
   return (
     <Box sx={{
-      backgroundColor: 'primary.dark',
       width: '100%',
       height: (theme) => theme.trello.boardBarHeight,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 2,
+      paddingX: 2,
+      overflowX: 'auto',
+      borderTop: '1px solid #00bfa5'
     }}>
-			Board Bar
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          sx={MENU_STYLES}
+          icon={<DashboardIcon />}
+          label="TrungQuanDev MERN Stack Board"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<VpnLockIcon />}
+          label="Public/Private Workspace"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<AddToDriveIcon />}
+          label="Add To Google Drive"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<FilterListIcon />}
+          label="Filters"
+          clickable
+        />
+      </Box>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Button variant='outlined' startIcon={<PersonAddIcon />}>Invite</Button>
+        <AvatarGroup
+          max={7}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: 16
+            }
+          }}
+        >
+          <Tooltip title="trungquandev">
+            <Avatar alt="trungquandev"
+              src='https://trungquandev.com/wp-content/uploads/2023/05/main-avatar-circle-min-trungquandev-codetq.jpeg'
+            />
+          </Tooltip>
+          <Tooltip title="trungquandev">
+            <Avatar alt="trungquandev"
+              src='https://trungquandev.com/wp-content/uploads/2021/01/trungquandev-avatar-2021.jpg'
+            />
+          </Tooltip>
+          <Tooltip title="trungquandev">
+            <Avatar alt="trungquandev"
+              src='https://trungquandev.com/wp-content/uploads/2018/04/trungquandev-avatar.jpeg'
+            />
+          </Tooltip>
+          <Tooltip title="trungquandev">
+            <Avatar alt="trungquandev"
+              src='https://trungquandev.com/wp-content/uploads/2019/03/trungquandev-avatar-01-scaled.jpg'
+            />
+          </Tooltip>
+          <Tooltip title="trungquandev">
+            <Avatar alt="trungquandev"
+              src='https://trungquandev.com/wp-content/uploads/2017/03/aboutme.jpg'
+            />
+          </Tooltip>
+          <Tooltip title="trungquandev">
+            <Avatar alt="trungquandev"
+              src='https://trungquandev.com/wp-content/uploads/2019/06/trungquandev-cat-avatar.png'
+            />
+          </Tooltip>
+
+          <Tooltip title="trungquandev">
+            <Avatar alt="trungquandev"
+              src='https://trungquandev.com/wp-content/uploads/2023/05/main-avatar-circle-min-trungquandev-codetq.jpeg'
+            />
+          </Tooltip>
+          <Tooltip title="trungquandev">
+            <Avatar alt="trungquandev"
+              src='https://trungquandev.com/wp-content/uploads/2021/01/trungquandev-avatar-2021.jpg'
+            />
+          </Tooltip>
+          <Tooltip title="trungquandev">
+            <Avatar alt="trungquandev"
+              src='https://trungquandev.com/wp-content/uploads/2018/04/trungquandev-avatar.jpeg'
+            />
+          </Tooltip>
+          <Tooltip title="trungquandev">
+            <Avatar alt="trungquandev"
+              src='https://trungquandev.com/wp-content/uploads/2019/03/trungquandev-avatar-01-scaled.jpg'
+            />
+          </Tooltip>
+          <Tooltip title="trungquandev">
+            <Avatar alt="trungquandev"
+              src='https://trungquandev.com/wp-content/uploads/2017/03/aboutme.jpg'
+            />
+          </Tooltip>
+          <Tooltip title="trungquandev">
+            <Avatar alt="trungquandev"
+              src='https://trungquandev.com/wp-content/uploads/2019/06/trungquandev-cat-avatar.png'
+            />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
