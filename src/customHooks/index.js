b366727718eca716037807128f4401ea5b508009
quanganh2001 <cocoi5659@gmail.com>
2024-07-23 09:56:55 +0700

    64.1 Fix Bug kéo thả khi cần bôi đen Text bằng chuột

---
+++ src/customHooks/index.js
@@ -0,0 +1,4 @@
+/**
+ * Youtube: Trungquandev - Một Lập Trình Viên
+ * Created by trungquandev.com's author on Nov 12, 2023
+ */
--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -177,6 +177,7 @@ function Column({ column }) {
                 size='small'
                 variant='outlined'
                 autoFocus
+                data-no-dnd="true"
                 value={newCardTitle}
                 onChange={(e) => setNewCardTitle(e.target.value)}
                 sx={{
@@ -198,6 +199,7 @@ function Column({ column }) {
               />
               <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
                 <Button
+                  data-no-dnd="true"
                   onClick={addNewCard}
                   variant='contained' color='success' size='small'
                   sx={{
